6e98aa78ac9e6afadb4b81375429c3a6f65ead7c
LMike54 <l.mike.9735@gmail.com>
2020-02-19 10:22:19 +0100

    feat(weather): review weatherAction

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = "http://api.openweathermap.org/data/2.5";
+const appId   = "appid=16306ca76c4af3dd905386e107bd0690";
+
 export function getWeather(){
-    return axios.get("https://api.openweathermap.org/data/2.5/weather?q=sidney&appid=16306ca76c4af3dd905386e107bd0690")
+    return axios.get(baseUrl+"/weather?q=sidney&"+appId);
 }
 
