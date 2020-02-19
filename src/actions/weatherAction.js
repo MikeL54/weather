a28a0f624d997d007bc190d17d4347aaa8f6f4ad
LMike54 <l.mike.9735@gmail.com>
2020-02-19 19:24:14 +0100

    feat(weather): everything including exe3

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
+// Requete GET sur http://api.openweathermap.org/data/2.5/weather?q=London&appid=e14154b4f09fc841a46c2fd919707747
+//Axios pour faire ma requete
+
 const baseUrl = "http://api.openweathermap.org/data/2.5";
-const appId   = "appid=16306ca76c4af3dd905386e107bd0690";
+const appId= "&appid=e14154b4f09fc841a46c2fd919707747";
 
-export function getWeather(city){
-
-    return axios.get(baseUrl+"/weather?q="+city+"&"+appId);
+export function getWeather(){
+    return axios.get(baseUrl+"/weather?q=London"+appId);
 }
 
+export function getWeatherByCity(city){
+    return axios.get(baseUrl+"/weather?q="+city+appId);
+}
+
+//Requete ajax get current weather by longitude et latitude
+export function getWeatherByCoords(coords){
+    return axios.get(baseUrl+"/weather?lat="+coords.latitude+"&lon="+coords.longitude+appId);
+}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,57 +1,72 @@
-import React, {useState, useEffect} from 'react'
-import {getWeather} from '../actions/weatherAction.js'
+import React, { useState, useEffect } from 'react'
+import { getWeather, getWeatherByCity, getWeatherByCoords } from '../actions/weatherAction';
+// import { mockWeather } from '../mocks/mockWeather';
 
 function Weather() {
-    const [weather, setWeather] = useState(null);
-    const [city, setCity] = useState("London");
 
-    
+    // const [weather, setWeather] = useState(mockWeather);
+    const [weather, setWeather] = useState(null);
+    const [city, setCity] = useState(null);
+
+    //Use Effect => Le composant est chargé
+    // => Le state est modifié (géré par [])
+    useEffect(()=>{
+        //Récupérer les cordonnées 
+        navigator.geolocation.getCurrentPosition(loadWeatherData,errorLoadWeatherData);
+    }, [])
 
     function kelvinToCelsius(tempKelvin){
         return Math.round(tempKelvin - 273.15);
     }
 
-    async function loadWeather(){
-        const weatherAjax = await getWeather(city);
+    // Weather par city avec la barre de recherche
+    async function searchWeatherByCity(){
+        const weatherAjaxByCity = await getWeatherByCity(city);
+        setWeather(weatherAjaxByCity.data);
+    }
+
+    function handleChange(event){
+        setCity(event.target.value);
+    }
+
+    //Weather par défaut
+    async function loadWeatherData(pos){
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
+        const weatherAjaxByCoords = await getWeatherByCoords(pos.coords);
+        setWeather(weatherAjaxByCoords.data);
+    }
+
+    async function errorLoadWeatherData(error){
+        const weatherAjax = await getWeather();
         setWeather(weatherAjax.data);
     }
 
-    useEffect(()=>{
-        loadWeather();
-    }, [city])
-
-    function handleChangeCity(city){
-        setCity(city);
-    }
-
     function loadIconWeather(idIcon){
-        return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png";
+       return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png"
     }
+
 
     return (
         <div>
-
-            {
-                weather ? 
-                <div>
-                    <h1>Météo : {weather.name}</h1>
-                    <input type="text" placeholder="Enter a city" onChange = {(event) => {handleChangeCity(event.target.value)}}/>
-                    <img src={loadIconWeather(weather.weather[0].icon)} alt="Not Found"/>
-                    <p>{weather.weather[0].description}</p>
-                    <p>{kelvinToCelsius(weather.main.temp)} °C</p>
-                    <p>Precipitation :  {weather.main.humidity}%</p>
-                    <p>Wind speed : {weather.wind.speed}mph</p>
-                    <p>{kelvinToCelsius(weather.main.feels_like)} °C</p>
-                </div>
-
-                :
-                <div>
-                    <h1>Météo en attente de chargement</h1>
-                </div>
-
+            { weather ? 
+            <div>
+                <input type="text" onChange={handleChange} />
+                <input type="button" onClick={searchWeatherByCity} value="Rechercher" />
+                <h1>Météo : {weather.name}</h1> 
+                <img alt="" src={loadIconWeather(weather.weather[0].icon)}></img>
+                <p>{weather.weather[0].description}</p>
+                <p>{kelvinToCelsius(weather.main.temp)} C°</p>
+                <p>{weather.main.humidity} %</p>
+                <p>{weather.wind.speed} m/s</p>
+                <p>{kelvinToCelsius(weather.main.feels_like)} C°</p>
+            </div>
+             : <div>
+                 <h1>Météo en attente de chargement</h1>
+               </div>
             }
-        </div>
 
+        </div>
     )
 }
 
