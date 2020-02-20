bbf551fa6938146745c9dd7d9b04a60229b5f607
LMike54 <l.mike.9735@gmail.com>
2020-02-20 15:51:57 +0100

    pouet

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -7,14 +7,19 @@ const baseUrl = "http://api.openweathermap.org/data/2.5";
 const appId= "&appid=e14154b4f09fc841a46c2fd919707747";
 
 export function getWeather(){
-    return axios.get(baseUrl+"/weather?q=London"+appId);
+    return axios.get(baseUrl+"/weather?q=London&cnt=4"+appId);
 }
 
-export function getWeatherByCity(city){
-    return axios.get(baseUrl+"/weather?q="+city+appId);
+export function getWeatherByCity(city, lang){
+    return axios.get(baseUrl+"/weather?q="+city+appId+"&lang="+lang);
 }
 
 //Requete ajax get current weather by longitude et latitude
-export function getWeatherByCoords(coords){
-    return axios.get(baseUrl+"/weather?lat="+coords.latitude+"&lon="+coords.longitude+appId);
+export function getWeatherByCoords(coords, lang){
+    return axios.get(baseUrl+"/weather?lat="+coords.latitude+"&lon="+coords.longitude+appId+"&lang="+lang);
+}
+
+// forecast 4days
+export function getForecastWeather(coords){
+    return axios.get(baseUrl+"/forecast/daily?q=London&cnt=4"+appId);
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,73 +1,138 @@
-import React, { useState, useEffect } from 'react'
-import { getWeather, getWeatherByCity, getWeatherByCoords } from '../actions/weatherAction';
+import React, { useState, useEffect } from "react";
+import {
+  getWeather,
+  getWeatherByCity,
+  getWeatherByCoords,
+  getForecastWeather
+} from "../actions/weatherAction";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
+
 // import { mockWeather } from '../mocks/mockWeather';
 
 function Weather() {
+  // const [weather, setWeather] = useState(mockWeather);
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState(null);
+  const [lang, setLang] = useState("en");
+  const [coord, setCoord] = useState(null);
+  const [isCoord, setIsCoord] = useState(true);
+  
+  const classes = useStyles();
 
-    // const [weather, setWeather] = useState(mockWeather);
-    const [weather, setWeather] = useState(null);
-    const [city, setCity] = useState(null);
+  //Use Effect => Le composant est chargé
+  // => Le state est modifié (géré par [])
+  useEffect(() => {
+    //Récupérer les cordonnées
+    navigator.geolocation.getCurrentPosition(
+      loadWeatherData,
+      errorLoadWeatherData
+    );
+  }, []);
 
-    //Use Effect => Le composant est chargé
-    // => Le state est modifié (géré par [])
-    useEffect(()=>{
-        //Récupérer les cordonnées 
-        navigator.geolocation.getCurrentPosition(loadWeatherData,errorLoadWeatherData);
-    }, [])
+  function kelvinToCelsius(tempKelvin) {
+    return Math.round(tempKelvin - 273.15);
+  }
 
-    function kelvinToCelsius(tempKelvin){
-        return Math.round(tempKelvin - 273.15);
-    }
+  // Weather par city avec la barre de recherche
+  async function searchWeatherByCity() {
+    setIsCoord(false);
+    const weatherAjaxByCity = await getWeatherByCity(city, lang);
+    setWeather(weatherAjaxByCity.data);
+  }
 
-    // Weather par city avec la barre de recherche
-    async function searchWeatherByCity(){
-        const weatherAjaxByCity = await getWeatherByCity(city);
-        setWeather(weatherAjaxByCity.data);
-    }
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
 
-    function handleChange(event){
-        setCity(event.target.value);
-    }
+  function handleChangeLang(value) {
+    setLang(value);
+    //Si le mec a pas fait de recherches
+    if (!setIsCoord) loadWeatherData(coord);
+  }
 
-    //Weather par défaut
-    async function loadWeatherData(pos){
-        console.log(pos.coords.latitude);
-        console.log(pos.coords.longitude);
-        const weatherAjaxByCoords = await getWeatherByCoords(pos.coords);
-        setWeather(weatherAjaxByCoords.data);
-    }
+  //Weather par défaut
+  async function loadWeatherData(pos) {
+    const weatherAjaxByCoords = await getWeatherByCoords(pos.coords, lang);
+    setWeather(weatherAjaxByCoords.data);
+    setCoord(pos);
+  }
 
-    async function errorLoadWeatherData(error){
-        const weatherAjax = await getWeather();
-        setWeather(weatherAjax.data);
-    }
+  async function errorLoadWeatherData(error) {
+    const weatherAjax = await getWeather();
+    setWeather(weatherAjax.data);
+  }
 
-    function loadIconWeather(idIcon){
-       return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png"
-    }
+  function loadIconWeather(idIcon) {
+    return "http://openweathermap.org/img/wn/" + idIcon + "@2x.png";
+  }
 
-
-    return (
+  return (
+    <div>
+      {weather ? (
         <div>
-            { weather ? 
-            <div>
-                <input type="text" onChange={handleChange} />
-                <input type="button" onClick={searchWeatherByCity} value="Rechercher" />
-                <h1>Météo : {weather.name}</h1> 
-                <img alt="" src={loadIconWeather(weather.weather[0].icon)}></img>
-                <p>{weather.weather[0].description}</p>
-                <p>{kelvinToCelsius(weather.main.temp)} C°</p>
-                <p>{weather.main.humidity} %</p>
-                <p>{weather.wind.speed} m/s</p>
-                <p>{kelvinToCelsius(weather.main.feels_like)} C°</p>
-            </div>
-             : <div>
-                 <h1>Météo en attente de chargement</h1>
-               </div>
-            }
+          <input type="text" onChange={handleChange} />
+          <input
+            type="button"
+            onClick={searchWeatherByCity}
+            value="Rechercher"
+          />
+          <h1>Météo : {weather.name}</h1>
+          <div className={classes.root}>
+            <ButtonGroup
+              color="secondary"
+              aria-label="outlined secondary button group"
+            >
+              <Button
+                onClick={() => {
+                  handleChangeLang("en");
+                }}
+              >
+                Anglais
+              </Button>
+              <Button
+                onClick={() => {
+                  handleChangeLang("fr");
+                }}
+              >
+                Français
+              </Button>
+              <Button
+                onClick={() => {
+                  handleChangeLang("ja");
+                }}
+              >
+                Japonais
+              </Button>
+            </ButtonGroup>
+          </div>
 
+          <img alt="" src={loadIconWeather(weather.weather[0].icon)}></img>
+          <p>{weather.weather[0].description}</p>
+          <p>{kelvinToCelsius(weather.main.temp)} C°</p>
+          <p>{weather.main.humidity} %</p>
+          <p>{weather.wind.speed} m/s</p>
+          <p>{kelvinToCelsius(weather.main.feels_like)} C°</p>
         </div>
-    )
+      ) : (
+        <div>
+          <h1>Météo en attente de chargement</h1>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
